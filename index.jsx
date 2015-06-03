c228b1fc080da0f3f7ab1797b4f5558ccf3e9480
frenchdonuts <jonathantan1991@gmail.com>
2015-06-03 15:58:02 -0700

    FEATURE: Start/Stop the stopwatch.

--- index.jsx
+++ index.jsx
@@ -1,18 +1,114 @@
+import Rx from 'rx';
 import React from 'react';
 import Tracker from './src/tracker';
 import EventEmitter from './src/utils/tracker_events';
-import shouldCheck from './src/utils/random_interval';
+import Intents from './src/intents';
 
-setInterval(() => EventEmitter.emit("tick"), 1000);
+var data = {};
 
-EventEmitter.on("tick", () => {
-  if (shouldCheck.next()) {
-    // captureScreenshot();
-    // askForDescription();
-  }
-});
+Intents.toggleStart.onNext(true);
+/* Define time streams */
+// let stream_tick = Rx.Observable.interval(100 /* ms */)
+// .pausable(Intents.toggleStart);
+//Hmmm...I could just make a new operator that keeps it constant
+// Rename stream_toggledTick
+let stream_tick = Rx.Observable.combineLatest(
+    Rx.Observable.interval(100 /* ms */),
+    Intents.toggleStart,
+    (tick, start) => start)
+  .scan(0, (acc, start) => {
+    // console.log(acc);
+    if (start) {
+      return acc + 1;
+    } else {
+      return acc;
+    }});
 
+let stream_deciSeconds = stream_tick
+  // reset when we hit a second
+  .map((x) => (x % 10))
+  .startWith(0);
+
+let stream_seconds = stream_tick
+  // emit only every second
+  .filter((x) => (x % 10 === 0))
+  // convert to seconds
+  .map((x) => (x / 10))
+  // reset when we hit a minute
+  .map((x) => (x % 60))
+  .startWith(0);
+
+let stream_minutes = stream_tick
+  // emit only every minute
+  .filter((x) => (x % 600 === 0))
+  // convert to minutes
+  .map((x) => (x / 600))
+  // reset when we hit an hour
+  .map((x) => (x % 60))
+  .startWith(0);
+
+let stream_hours = stream_tick
+  // emit only every hour
+  .filter((x) => (x % 36000 === 0))
+  // convert to hours
+  .map((x) => (x / 36000))
+  .startWith(0);
+
+data.stream_stopWatchState = 
+    Rx.Observable.combineLatest([
+        stream_hours,
+        stream_minutes,
+        stream_seconds,
+        stream_deciSeconds],
+        (hours, minutes, seconds, deciSeconds) => {
+          return {
+            hours: hours,
+            minutes: minutes,
+            seconds: seconds,
+            deciSeconds, deciSeconds
+          };
+        });
+data.stream_Running = 
+  Intents.toggleStart.map((x) => { return { running: x }; });
+
+/* Define random interval streams */
+let lowerBound = 10 /* min */
+let upperBound = 60 /* min */
+let stream_randomIntervals = stream_minutes
+  .scan(randomIntervalBetween(lowerBound, upperBound), (acc, x) => {
+    if (acc === -1)
+      return randomIntervalBetween(lowerBound, upperBound);
+    else
+      return acc - 1;
+  });
+let stream_shouldCheckOnUser = stream_randomIntervals
+  .map((x) => {
+    if (x === 0)
+      return true;
+    else 
+      return false;
+  });
+
+stream_shouldCheckOnUser.subscribe(
+    (x) => {
+      if (x) {
+        console.log("Take a screenshot.");
+        // take screenshot
+        // ask for description
+      } else {
+        // console.log("Do nothing");
+        // do nothing
+      }
+    },
+    (err) => {
+      console.log("Error: " + err);
+    });
+
+function randomIntervalBetween(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+/* The renderz */
 React.render(
-  <Tracker events={EventEmitter}/>,
-  document.getElementById('tracker')
-)
+  <Tracker data={data}/>,
+  document.getElementById('tracker'));
--- src/tracker.jsx
+++ src/tracker.jsx
@@ -1,4 +1,6 @@
+import Rx from 'rx';
 import React from 'react';
+import Intents from './intents';
 
 class Tracker extends React.Component {
   constructor(props) {
@@ -6,9 +8,9 @@ class Tracker extends React.Component {
   }
 
   componentDidMount() {
-    this.props.events.on("start", this.start.bind(this));
-    this.props.events.on("stop", this.stop.bind(this));
-    this.props.events.on("reset", this.reset.bind(this));
+    // this.props.events.on("start", this.start.bind(this));
+    // this.props.events.on("stop", this.stop.bind(this));
+    // this.props.events.on("reset", this.reset.bind(this));
   }
 
   start() {
@@ -24,51 +26,80 @@ class Tracker extends React.Component {
   }
 
   render() {
-    return <Stopwatch events={this.props.events}/>;
+    return <Stopwatch data={this.props.data}/>;
   }
 }
 
 class Stopwatch extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { elapsed: 0 };
+    this.state = {
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+      deciSeconds: 0
+    };
   }
 
   componentDidMount() {
-    this.props.events.on("tick", this.tick.bind(this));
+    this.stateSubscription = this.props.data.stream_stopWatchState.subscribe(
+        (state) => this.setState(state));
   }
 
   componentWillUnmount() {
-    //clearInterval(this.timer);
-  }
-
-  tick() {
-    this.setState({elapsed: this.state.elapsed + 1});
+    this.stateSubscription.unsubscribe(); 
   }
 
   render() {
-    var elapsed = this.state.elapsed;
+    // var elapsed = this.state.elapsed;
 
-    var seconds = (elapsed / 10).toFixed(1);
+    // var seconds = (elapsed / 10).toFixed(1);
 
     return <div>
-            <h3>{elapsed} seconds</h3>
+            <span>{this.state.hours} : {this.state.minutes} : {this.state.seconds} : {this.state.deciSeconds}</span>
             <span>
-              <TheButton events={this.props.events}/>
+              <StartStopBtn data={this.props.data}/>
+              <ResetBtn />
             </span>
            </div>
   }
 }
 
-class TheButton extends React.Component {
+class StartStopBtn extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { running: false };
+    this.onToggleStart = this.onToggleStart.bind(this);
+  }
+
+  componentDidMount() {
+    this.stateSubscription = this.props.data.stream_Running.subscribe(
+        (state) => this.setState(state));
+  }
+
+  componentWillUnmount() {
+    this.stateSubscription.unsubscribe();
+  }
+
+  onToggleStart(event) {
+    // console.log("Toggle!");
+    // console.log(this.state.running);
+    Intents.toggleStart.onNext(!this.state.running);
+  }
+
+  startOrStop() {
+    return this.state.running ? "Stop" : "Start";
   }
 
   render() {
-    return <h7>Start</h7>
+    return <button onClick={this.onToggleStart}>{this.startOrStop()}</button>
+  }
+}
+
+class ResetBtn extends React.Component {
+  render() {
+    return <h1>Reset</h1>
   }
 }
 
 export default Tracker;
-
